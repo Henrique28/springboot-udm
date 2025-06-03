01ffbc35bd4d2e891b7260b2ed84c185635599d6
henrique <henrique@teste.com>
2025-06-03 17:49:33 -0300

    Project created

---
+++ src/main/java/com/henrique/udm/UdmApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.udm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdmApplication.class, args);
+	}
+
+}
