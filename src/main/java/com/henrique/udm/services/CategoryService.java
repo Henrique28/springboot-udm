f968b87f49a11d6802005f31ee1e582037aa599d
henrique <henrique@teste.com>
2025-06-04 16:32:24 -0300

    Category

---
+++ src/main/java/com/henrique/udm/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.henrique.udm.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.henrique.udm.entities.Category;
+import com.henrique.udm.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
