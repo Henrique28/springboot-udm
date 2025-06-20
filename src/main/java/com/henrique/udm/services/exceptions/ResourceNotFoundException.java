5e853ff559366cc723a7f48920e6b58c8926a186
henrique <henrique@teste.com>
2025-06-20 10:51:54 -0300

    Exception handling - findById

---
+++ src/main/java/com/henrique/udm/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.henrique.udm.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+	
+}
