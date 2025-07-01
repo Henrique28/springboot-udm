89061917df1305f0ad20265fa7eef775295ff600
henrique <henrique@teste.com>
2025-07-01 10:33:39 -0300

    Exception handling - update

--- src/main/java/com/henrique/udm/services/UserService.java
+++ src/main/java/com/henrique/udm/services/UserService.java
@@ -12,6 +12,8 @@ import com.henrique.udm.repositories.UserRepository;
 import com.henrique.udm.services.exceptions.DatabaseException;
 import com.henrique.udm.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -41,15 +43,18 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-	}
-	
+	}	
 }
